921762dce2fa7a9d41ffc5a8f3e3cf8448de43e5
Aptir <cesar.arzapalo@unmsm.edu.pe>
2020-03-22 19:30:35 -0500

    AptirStore

---
+++ app/src/main/java/com/example/platzistore/AdapterLanding.kt
@@ -0,0 +1,29 @@
+package com.example.platzistore
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Adapter
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_landing.view.*
+
+class AdapterLanding(val data: List<ItemLanding>):RecyclerView.Adapter<AdapterLanding.Holder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder =
+        Holder(parent.inflate(R.layout.item_landing))
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder?.bindView(data[position])
+    }
+
+
+    override fun getItemCount(): Int = data.size
+
+    class Holder(itemView: View):RecyclerView.ViewHolder(itemView){
+        fun bindView(itemLanding: ItemLanding){
+            with(itemLanding){
+                itemView.txtTitleItem.text=title
+                itemView.txtDescItem.text=desc
+                itemView.txtPriceItem.text="$ ${String.format("%.2f",price)}"
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/platzistore/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.platzistore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.GridLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rcViewLanding.layoutManager=GridLayoutManager(this,2)
+        val itemShop=(0..20).map{
+            ItemLanding("Titulo $it","Descr $it",200.00+it)
+        }
+
+        val adapter = AdapterLanding(itemShop)
+
+        rcViewLanding.adapter=adapter
+    }
+}
